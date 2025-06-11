736e2ad22a4ddff596f3d478a24c9adfd9b7997c
pegucas <pedrogudincastanho@gmail.com>
2025-06-11 08:55:18 -0300

    criado

---
+++ AC de web desing/principal.js
@@ -0,0 +1,19 @@
+function Pedro() {
+    window.location.href = "pedro.html";
+    alert("Entrando...")
+}
+
+function Leo() {
+    window.location.href = "leonardo.html";
+    alert("Entrando...")
+}
+
+function Manu() {
+    window.location.href = "manuela.html";
+    alert("Entrando...")
+}
+
+function cachorro() {
+    window.location.href = "cachorro.html";
+    alert("Entrando...")
+}
